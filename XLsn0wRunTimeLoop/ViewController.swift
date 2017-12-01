dd6188e15cd7fb350cc22fdf6d674d425b9a830e
xlsn0w <xlsn0w@outlook.com>
2017-12-01 09:39:11 +0800

    init

---
+++ XLsn0wRunTimeLoop/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  XLsn0wRunTimeLoop
+//
+//  Created by golong on 2017/12/1.
+//  Copyright © 2017年 XLsn0w. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    func runloop() -> () {
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
