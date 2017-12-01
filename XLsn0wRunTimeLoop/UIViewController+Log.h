397b37d8add6045d6b98fba9c1637c91fb47d456
xlsn0w <xlsn0w@outlook.com>
2017-12-01 15:30:54 +0800

    update

--- XLsn0wRunTimeLoop/UIViewController+Log.h
+++ XLsn0wRunTimeLoop/UIViewController+Log.h
@@ -10,6 +10,9 @@
 
 @interface UIViewController (Log)
 
+//声明属性时，不要以new开头。如果非要以new开头命名属性的名字，需要自己定制get方法名，如
+@property(getter=theString) NSString *newString;
+
 @property (nonatomic, copy) NSString *name;///copy一份对象 深拷贝
 
 @end
